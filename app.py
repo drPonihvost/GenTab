861ca4a71f5066c69a7feb48f616bce97603d234
drPonihvost <kudrovpn@gmail.com>
2021-05-06 16:11:15 +0700

    Добавлена база данных для загрузки проектов

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-
 from blueprints.auth.routes import auth
 from blueprints.projects.routes import projects
 
@@ -12,8 +11,15 @@ CORS(app)
 app.register_blueprint(auth)
 app.register_blueprint(projects)
 
+# config
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base/gentab.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 jwt = JWTManager(app)
+
+
+
 
 if __name__ == "__main__":
     app.run()
---
+++ base/base.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+from app import app
+
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(225), unique=True)
+    # user_id = 0
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'<Project {self.name}, created {self.created_at}, updated{self.updated_at}>'
+
+
+class Object(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+    marker_id = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'<Object {self.name}, in project_id {self.project_id} >'
+
+
+class Marker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    object_id = db.Column(db.Integer, db.ForeignKey('object.id'))
+    name = db.Column(db.String(50))
+    allele_1 = db.Column(db.String(5))
+    allele_2 = db.Column(db.String(5))
+    allele_3 = db.Column(db.String(5))
+    allele_4 = db.Column(db.String(5))
+    allele_5 = db.Column(db.String(5))
+    allele_6 = db.Column(db.String(5))
+
+    def __init__(self, name, allele_1=None, allele_2=None, allele_3=None,
+                 allele_4=None, allele_5=None, allele_6=None):
+        self.name = name
+        self.allele_1 = allele_1
+        self.allele_2 = allele_2
+        self.allele_3 = allele_3
+        self.allele_4 = allele_4
+        self.allele_5 = allele_5
+        self.allele_6 = allele_6
+
+    def __repr__(self):
+        return f'<Marker {self.name}>'
